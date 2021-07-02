d0d56cafbc8861cdd689b8322f1f6314b8355f28
sunsiyuan <siyuan.sun1@baozun.com>
2021-07-02 11:15:31 +0800

    feat: change home page

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,23 +1,27 @@
 <template>
   <div class="home">主页,CI测试</div>
+  <div>
+    测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试
+  </div>
+  <img alt="Vue logo" src="~@/assets/logo.png" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import http from '@/common/axios/index'
+// import http from '@/common/axios/index'
 
 export default defineComponent({
   name: 'Home',
   components: {},
   setup() {
-    http
-      .get('/users/XPoet')
-      .then((res) => {
-        console.log(res)
-      })
-      .catch((er) => {
-        console.log(er)
-      })
+    // http
+    //   .get('/users/XPoet')
+    //   .then((res) => {
+    //     console.log(res)
+    //   })
+    //   .catch((er) => {
+    //     console.log(er)
+    //   })
   }
 })
 </script>

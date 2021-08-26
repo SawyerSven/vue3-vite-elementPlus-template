c120bac34debb5fd0727771d6cf2c6fa27effb37
sunsiyuan <siyuan.sun1@baozun.com>
2021-08-26 16:46:11 +0800

    feat: add

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="container">
-    <ItemSwiperView></ItemSwiperView>
+    <img src="~@/assets/images/bg.webp" alt="" />
+    <img
+      src="https://scm-dam-oss-cn-cdn.baozun.com/scm-dam/2021-08-16/11w640_f5457e65-d060-4fde-a05a-72b1b9ff7a88.webp?x-oss-process=image/format,webp"
+      alt=""
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import ItemSwiperView from '@/components/item-swiper-view/index.vue'
 // import http from '@/common/axios/index'
 
 export default defineComponent({
   name: 'Home',
-  components: { ItemSwiperView },
   setup() {
     // http
     //   .get('/users/XPoet')
